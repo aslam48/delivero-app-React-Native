6364499413c49200c117d1bfd40bd1bbce780693
aslam48 <youngfizzy001@gmail.com>
2023-01-24 16:35:00 +0100

    working

--- App.js
+++ App.js
@@ -27,10 +27,6 @@ export default function App() {
 
 
 
-// <SafeAreaView className="flex-1 items-center justify-center">
-//       <Text className="text-red-600">Open up </Text>
-//       <StatusBar style="auto" />
-// </SafeAreaView>
 
 
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,8 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Image, StatusBar } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { useLayoutEffect } from 'react'
+import { UserIcon, ChevronDownIcon, SearchIcon, AdjustmentsIcon } from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
     const navigation = useNavigation()
@@ -13,9 +14,26 @@ const HomeScreen = () => {
     },[])
 
   return (
-    <View>
-      <Text className="text-red-900">Open up </Text>
-    </View>
+    <SafeAreaView>
+      <StatusBar style="auto" />
+      
+
+
+      <Text className="text-red-900"> 
+
+      {/* header*/}
+
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image source={{uri:"https://links.papareact.com/wru",}} className="h-7 w-7 bg-gray-300 p-4 rounded-full"/>
+
+        <View>
+           <Text className="font-bold text-gray-400 text-xs">Deliver Now</Text>
+           <Text className="font-bold text-xl"> Current Location <ChevronDownIcon size={20} color="#00ccbb" />  </Text>
+        </View>
+      </View>
+
+       </Text>
+    </SafeAreaView>
   )
 }
 
